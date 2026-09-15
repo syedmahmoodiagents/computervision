09ecab2edc879585eba37c69ffb8d60df4aff204
smahmood <syedmahmood.iagents@gmail.com>
2026-09-15 17:43:13 +0530

    Add files via upload

--- MTCNN/infervgg.py
+++ MTCNN/infervgg.py
@@ -3,7 +3,7 @@ from torchvision import models
 import streamlit as st
 import cv2
 import numpy as np
-import mtcnn
+from mtcnn import MTCNN
 
 m = models.vgg16(weights=None)
 m.classifier[6] = torch.nn.Linear(in_features=4096, out_features=2, bias=True)
@@ -22,7 +22,7 @@ for param in m.classifier[6].parameters():
 
 def crop_faces(image_path, output_prefix="face"):
 
-    detector = mtcnn.MTCNN()
+    detector = MTCNN()
     image = cv2.imread(image_path)
 
     if image is not None:
@@ -40,11 +40,17 @@ imgfile = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 if imgfile is not None:
     file_bytes = imgfile.read()
     npimg = cv2.imdecode(np.frombuffer(file_bytes, np.uint8), cv2.IMREAD_COLOR)
-    
-    rimg = crop_faces(npimg)
-    st.image(rimg) # display the image in the app
+    # image_rgb = cv2.cvtColor(npimg, cv2.COLOR_BGR2RGB)
+    detector = MTCNN()
+    detections = detector.detect_faces(npimg)
 
-    resized = cv2.resize(rimg, (224, 224))
+    x, y, width, height = detections[0]['box']
+    x, y = max(0, x), max(0, y)
+    cropped_face = npimg[y:y+height, x:x+width]
+
+    # st.image(cropped_face, channels="RGB")
+    
+    resized = cv2.resize(cropped_face, (224, 224))
     imag_f = resized.reshape(1, 3, 224, 224)
     tor_image = torch.FloatTensor(imag_f)
 
@@ -53,6 +59,7 @@ if imgfile is not None:
         Yp = m(tor_image)
         sx = torch.softmax(Yp, dim=1)
         predicted_img = torch.argmax(sx, dim=1)
+        
         if predicted_img.item() == 0:
             st.title("Predicted class: Tom")
         else:
